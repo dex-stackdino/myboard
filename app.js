a67c585524c0d2f59dc8bf422c4a60b421191556
dex-stackdino <102156055+dex-stackdino@users.noreply.github.com>
2022-03-22 18:26:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express(); //Initialized and server ready
 
 app.use(express.static("public"));
 
-let port = process.env.PORT || 5000;
+let port = process.env.PORT ;
 let server = app.listen(port, () => {
-    console.log("Listening to port" + port);
+    console.log("Listening to port " +  port);
 })
 
 let io = socket(server);
